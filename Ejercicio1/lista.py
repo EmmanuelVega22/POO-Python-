fbeb9394ddf633b447d144ccaaad834bded334d0
Emmanuel Vega <139393277+EmmanuelVega22@users.noreply.github.com>
2024-04-16 02:11:02 -0300

    Add files via upload

---
+++ Ejercicio1/lista.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 15 17:39:13 2024
+
+@author: Emmanuel
+"""
+
---
+++ Ejercicio1/main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Emmanuel
+"""
+
+import funciones
+if __name__=='__main__':
+    
+    lista = []
+
+        
+    funciones.test(lista)
+    
+    
