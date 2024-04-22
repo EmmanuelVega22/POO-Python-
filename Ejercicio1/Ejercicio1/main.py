2e809addbe881447688d6bb48d21f9825b0c5da1
Emmanuel Vega <139393277+EmmanuelVega22@users.noreply.github.com>
2024-04-22 00:31:36 -0300

    Add files via upload

---
+++ Ejercicio1/Ejercicio1/main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Emmanuel
+"""
+
+import funciones
+from clase import CajaDeAhorro
+
+if __name__=='__main__':
+    
+    lista = []
+
+    funciones.test(lista) 
+    
