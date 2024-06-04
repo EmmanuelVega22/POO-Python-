b1f92d17d753cf5910b3979450603e99e3228c75
Emmanuel Vega <139393277+EmmanuelVega22@users.noreply.github.com>
2024-06-04 00:58:04 -0300

    Add files via upload

---
+++ Practica5/LogiPack/config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+config.py:
+    
+@author: Emmanuel
+"""
+
+SECRET_KEY = "GDtfDCFYjD"
+SQLALCHEMY_DATABASE_URI = 'sqlite:///datos.sqlite3'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
