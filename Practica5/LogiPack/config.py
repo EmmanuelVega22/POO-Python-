c50e8b890943ff13434ed71a40673ee1b4055c00
Emmanuel Vega <139393277+EmmanuelVega22@users.noreply.github.com>
2024-06-12 21:40:58 -0300

    Delete Practica5/LogiPack directory

--- Practica5/LogiPack/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'datos.sqlite3')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
